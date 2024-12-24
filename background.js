a6ced9c4193dc6b0447813aee6b117e5124f0a2a
flycool <flycoolcool@163.com>
2024-12-24 17:18:38 +0800

    add icon

--- background.js
+++ background.js
@@ -1,9 +1,11 @@
 
 const proandroiddev = "https://proandroiddev.com/";
-const medium = "https://medium.com/androiddevelopers";
+const medium = "https://medium.com/androiddevelopers/";
+const kotlin_academy = "https://blog.kotlin-academy.com/";
 
 chrome.action.onClicked.addListener(async (tab) => {
-  if (tab.url.startsWith(proandroiddev) || tab.url.startsWith(medium)) {
+  if (tab.url.startsWith(proandroiddev) || tab.url.startsWith(medium) ||
+	  tab.url.startsWith(kotlin_academy)) {
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       files: ["parse.js"],
