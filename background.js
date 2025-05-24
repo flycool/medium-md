08b93a5779179db57e6874ae54bd638dc1b5b965
flycool <flycoolcool@163.com>
2025-05-24 15:51:43 +0800

    update includes url

--- background.js
+++ background.js
@@ -1,11 +1,13 @@
 
-const proandroiddev = "https://proandroiddev.com/";
-const medium = "https://medium.com/androiddevelopers/";
-const kotlin_academy = "https://blog.kotlin-academy.com/";
+//const proandroiddev = "https://proandroiddev.com/";
+//const medium = "https://medium.com/androiddevelopers/";
+//const kotlin_academy = "https://blog.kotlin-academy.com/";
+//const clean_android = "https://medium.com/clean-android-dev/";
+const proandroiddev = "proandroiddev.com";
+const medium = "medium.com";
 
 chrome.action.onClicked.addListener(async (tab) => {
-  if (tab.url.startsWith(proandroiddev) || tab.url.startsWith(medium) ||
-	  tab.url.startsWith(kotlin_academy)) {
+  if (tab.url.includes(proandroiddev) || tab.url.includes(medium)) {
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       files: ["parse.js"],
